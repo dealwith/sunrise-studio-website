5bcdd4ee9f9a42ce8716e51685f883be040321a2
dealwith <gleb.krishin@icloud.com>
2024-04-09 20:01:55 +0200

    Change text to developers

--- app/components/OurTeamsExperienceSection/OurTeamsExperienceSection.tsx
+++ app/components/OurTeamsExperienceSection/OurTeamsExperienceSection.tsx
@@ -5,7 +5,7 @@ export const OurTeamsExperienceSection = () => {
     <section className={styles.component}>
       <h2 className="text-[52px] font-unbounded font-light">
         Our team&apos;s experience helps you find the best{" "}
-        <div className={styles.textBg}>employees</div> around the world
+        <div className={styles.textBg}>developers</div> around the world
       </h2>
     </section>
   );
