f560c2f7561f4050c0e6ac8aa76d7e9356265333
Nicholas Jones <njonesgrad@gmail.com>
2024-07-12 19:24:56 +0200

    Fix NAV Home Link
    Remove the ROUTES as this triggered the element to be highlighted.

--- app/components/Navigation/Navigation.tsx
+++ app/components/Navigation/Navigation.tsx
@@ -1,4 +1,3 @@
-import { ROUTES } from "constants/index";
 import { LiWithActiveLink } from "components";
 
 import styles from "./navigation.module.scss";
@@ -7,7 +6,7 @@ export const Navigation = () => {
   return (
     <nav className="flex flex-col w-full justify-center items-center text-white">
       <ul className={styles.list}>
-        <LiWithActiveLink href={ROUTES.HOME}>Home</LiWithActiveLink>
+        <LiWithActiveLink href={"#home"}>Home</LiWithActiveLink>
         <LiWithActiveLink href={"#aboutUs"}>About us</LiWithActiveLink>
         <LiWithActiveLink href={"#services"}>Services</LiWithActiveLink>
         <LiWithActiveLink href={"#projects"}>Projects</LiWithActiveLink>
