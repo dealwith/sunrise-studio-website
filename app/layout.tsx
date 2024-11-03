f61fd3f7f96056b0902654f87f1ab404d1890e15
Nicholas Jones <njonesgrad@gmail.com>
2024-11-04 00:07:23 +0100

    Insert Google Tags (#43)

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ import "./styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 
 import styles from "./layout.module.scss";
+import Script from "next/script"; // Import Script from next/script
 
 export default function RootLayout({
   children,
@@ -50,6 +51,20 @@ export default function RootLayout({
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
+
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=GTM-P42N8W6R'+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P42N8W6R');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body
         className={cn(
@@ -58,6 +73,17 @@ export default function RootLayout({
           isActiveBurger && "overflow-hidden",
         )}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P42N8W6R"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <BurgerContext.Provider value={{ isActiveBurger, setIsActiveBurger }}>
           <Header />
           {children}
