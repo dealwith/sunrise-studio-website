8b4d351729b19c3097f83fe1a6c2a601ae4e267b
Grappler2010 <yakhyaev2008@mail.ru>
2024-11-15 19:52:11 +0300

    Add the calendly component and responsive

--- app/components/StaffingSectionContent/StaffingSectionContent.tsx
+++ app/components/StaffingSectionContent/StaffingSectionContent.tsx
@@ -1,27 +1,20 @@
-import { H2, Section, UnderlineText } from "components";
-import Script from "next/script";
+import { CalendlyWidget, Section, UnderlineText } from "components";
 
 export const StaffingSectionContent = () => {
   return (
     <Section>
-      <div className="flex gap-10 items-center">
+      <div className="flex gap-8 items-center md:flex-row flex-col">
         <UnderlineText
           text="Get the best developers, without all the stress"
           isRedText={false}
+          textClassName="md:text-start text-center"
           imageClassName="mt-6"
         />
-        <div className="w-full py-[60px] px-4 flex flex-col items-center gap-[60px] rounded-2xl bg-primary bg-opacity-50">
-          <H2>Get Started</H2>
-          <div
-            className="calendly-inline-widget"
-            data-url="https://calendly.com/sunrisestudioworkspace?hide_gdpr_banner=1&primary_color=ff7600"
-            style={{ minWidth: "500px", height: "700px" }}
-          />
-          <Script
-            type="text/javascript"
-            src="https://assets.calendly.com/assets/external/widget.js"
-            async
-          />
+        <div className="w-full lg:px-[60px] px-6 pt-6 pb-14 flex flex-col items-center gap-3 rounded-2xl bg-primary bg-opacity-50">
+          <h2 className="md:text-3xl text-2xl text-center font-unbounded">
+            Schedule a call with us
+          </h2>
+          <CalendlyWidget />
         </div>
       </div>
     </Section>
