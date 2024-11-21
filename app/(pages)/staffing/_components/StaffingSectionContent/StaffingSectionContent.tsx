15729b4d10765ca9218a85812f45abbdf54541cd
Grappler2010 <yakhyaev2008@mail.ru>
2024-11-21 17:29:32 +0300

    Change the CalendlyWidget styles

--- app/(pages)/staffing/_components/StaffingSectionContent/StaffingSectionContent.tsx
+++ app/(pages)/staffing/_components/StaffingSectionContent/StaffingSectionContent.tsx
@@ -3,15 +3,15 @@ import { CalendlyWidget, Section, UnderlineText } from "components";
 export const StaffingSectionContent = () => {
   return (
     <Section>
-      <div className="flex gap-8 items-center md:flex-row flex-col">
+      <div className="flex md:gap-8 gap-4 items-center md:flex-row flex-col">
         <UnderlineText
           text="Get the best developers, without all the stress"
           isRedText={false}
           textClassName="md:text-start text-center"
-          imageClassName="mt-6"
+          imageClassName="md:mt-6 mt-3"
         />
-        <div className="w-full lg:px-[60px] px-6 pt-6 pb-14 flex flex-col items-center gap-3 rounded-2xl bg-primary bg-opacity-50">
-          <h2 className="md:text-3xl text-2xl text-center font-unbounded">
+        <div className="flex pt-6 flex-col items-center gap-3 rounded-2xl bg-primary max-w-[500px] w-full">
+          <h2 className="md:text-3xl text-2xl text-center font-unbounded px-3">
             Schedule a call with us
           </h2>
           <CalendlyWidget />
--- app/components/CalendlyWidget/CalendlyWidget.tsx
+++ app/components/CalendlyWidget/CalendlyWidget.tsx
@@ -4,9 +4,9 @@ export const CalendlyWidget = () => {
   return (
     <>
       <div
-        className="calendly-inline-widget rounded-2xl overflow-hidden"
+        className="calendly-inline-widget rounded-b-2xl overflow-hidden"
         data-url="https://calendly.com/sunrisestudioworkspace?background_color=1c1c1e&text_color=ffffff&primary_color=e95848&display_scheduled_event_end=true&hide_event_type_details=false&hide_gdpr_banner=true"
-        style={{ maxWidth: "531px", height: "336px", width: "100%" }}
+        style={{ width: "100%", height: "40vh", margin: "16px 0 16px 0" }}
       />
       <Script
         type="text/javascript"
