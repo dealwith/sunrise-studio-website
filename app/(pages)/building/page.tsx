6c207329f0345eb3f65abbb5125b664e355e8683
dealwith <gleb.krishin@icloud.com>
2024-12-01 21:04:30 +0100

    check

--- app/(pages)/building/page.tsx
+++ app/(pages)/building/page.tsx
@@ -15,6 +15,8 @@ import { useSmoothScroll } from "hooks";
 export default function Building() {
   const { elementRef } = useSmoothScroll();
 
+  console.log("commit");
+
   return (
     <>
       <main
