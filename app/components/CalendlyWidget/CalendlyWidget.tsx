e99a040eb56a53a57453cc32a397f99f51508b00
Grappler2010 <yakhyaev2008@mail.ru>
2024-12-02 21:54:47 +0300

    Fix loading state for calendly-widget

--- app/components/CalendlyWidget/CalendlyWidget.tsx
+++ app/components/CalendlyWidget/CalendlyWidget.tsx
@@ -1,8 +1,16 @@
 import Script from "next/script";
+import { useState } from "react";
 
 export const CalendlyWidget = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <>
+      {isLoading && (
+        <p className="lg:text-2xl md:text-xl text-base font-normal mt-4">
+          Loading Calendar Widget...
+        </p>
+      )}
       <div
         className="calendly-inline-widget rounded-b-2xl overflow-hidden"
         data-url="https://calendly.com/sunrisestudioworkspace?background_color=1c1c1e&text_color=ffffff&primary_color=e95848&display_scheduled_event_end=true&hide_event_type_details=false&hide_gdpr_banner=true"
@@ -12,6 +20,7 @@ export const CalendlyWidget = () => {
         type="text/javascript"
         src="https://assets.calendly.com/assets/external/widget.js"
         async
+        onLoad={() => setIsLoading(false)}
       />
     </>
   );
