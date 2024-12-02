4264aa1a5414e3e5f7cb147a423cb98c2957f34d
Grappler2010 <yakhyaev2008@mail.ru>
2024-12-02 22:23:47 +0300

    Add the InlineWidget from react-calendly

--- app/components/CalendlyWidget/CalendlyWidget.tsx
+++ app/components/CalendlyWidget/CalendlyWidget.tsx
@@ -1,26 +1,18 @@
-import Script from "next/script";
-import { useState } from "react";
+import { InlineWidget } from "react-calendly";
 
 export const CalendlyWidget = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const LoadingSpinner = () => (
+    <div className="text-base font-normal flex justify-center">
+      Loading Calendar...
+    </div>
+  );
 
   return (
     <>
-      {isLoading && (
-        <p className="lg:text-2xl md:text-xl text-base font-normal mt-4">
-          Loading Calendar Widget...
-        </p>
-      )}
-      <div
-        className="calendly-inline-widget rounded-b-2xl overflow-hidden"
-        data-url="https://calendly.com/sunrisestudioworkspace?background_color=1c1c1e&text_color=ffffff&primary_color=e95848&display_scheduled_event_end=true&hide_event_type_details=false&hide_gdpr_banner=true"
-        style={{ width: "100%", height: "40vh", margin: "16px 0 16px 0" }}
-      />
-      <Script
-        type="text/javascript"
-        src="https://assets.calendly.com/assets/external/widget.js"
-        async
-        onLoad={() => setIsLoading(false)}
+      <InlineWidget
+        url="https://calendly.com/sunrisestudioworkspace?background_color=1c1c1e&text_color=ffffff&primary_color=e95848&display_scheduled_event_end=true&hide_event_type_details=false&hide_gdpr_banner=true"
+        styles={{ width: "100%", height: "40vh", margin: "16px 0 16px 0" }}
+        LoadingSpinner={LoadingSpinner}
       />
     </>
   );
