ff624d4abaa4ea0213d91d2d9be6ad031a2d1ee1
Magomed Yakhyaev <59708232+Grappler2010@users.noreply.github.com>
2025-03-25 11:39:17 +0300

    Fix/staffing mobile navigation (#73)
    * Add the EcommercePricingPlansCard and EcommercePricingPlansHeader components
    
    * Add the CardTab and EcommercePricingPlansMobile components
    
    * Add the EcommercePricingPlansShowMore component
    
    * Fix laptop-m query
    
    * Remove unused useState
    
    * Add the our-team-slider in about-us page
    
    * Add the autofill for contact-us-form
    
    * Change the second and fourth images
    
    * Fix types for review
    
    * Fix build
    
    * Fix mobile-navigation in staffing-page

--- app/(pages)/staffing/_components/StaffingSectionContent/StaffingSectionContent.tsx
+++ app/(pages)/staffing/_components/StaffingSectionContent/StaffingSectionContent.tsx
@@ -1,22 +1,37 @@
-import { CalendlyWidget, Section, UnderlineText } from "components";
+import { useContext } from "react";
+
+import {
+  CalendlyWidget,
+  MobileNavigation,
+  Section,
+  UnderlineText,
+} from "components";
+import { BurgerContext } from "context";
+import { useWindowSize } from "hooks";
 
 export const StaffingSectionContent = () => {
+  const { isActiveBurger } = useContext(BurgerContext);
+  const { isLaptopS } = useWindowSize();
+
   return (
     <Section>
-      <div className="flex md:gap-8 gap-4 items-center md:flex-row flex-col">
-        <UnderlineText
-          text="Get the best developers, without all the stress"
-          isRedText={false}
-          textClassName="md:text-start text-center"
-          imageClassName="md:mt-6 mt-3"
-        />
-        <div className="flex p-5 flex-col items-center gap-3 rounded-2xl bg-primary bg-opacity-50 max-w-[500px] w-full mt-5 md:mt-5">
-          <h2 className="md:text-3xl text-1xl text-center font-unbounded px-3">
-            Schedule a call with us
-          </h2>
-          <CalendlyWidget />
+      {!isActiveBurger && (
+        <div className="flex md:gap-8 gap-4 items-center md:flex-row flex-col">
+          <UnderlineText
+            text="Get the best developers, without all the stress"
+            isRedText={false}
+            textClassName="md:text-start text-center"
+            imageClassName="md:mt-6 mt-3"
+          />
+          <div className="flex p-5 flex-col items-center gap-3 rounded-2xl bg-primary bg-opacity-50 max-w-[500px] w-full mt-5 md:mt-5">
+            <h2 className="md:text-3xl text-1xl text-center font-unbounded px-3">
+              Schedule a call with us
+            </h2>
+            <CalendlyWidget />
+          </div>
         </div>
-      </div>
+      )}
+      {isLaptopS && <MobileNavigation />}
     </Section>
   );
 };
